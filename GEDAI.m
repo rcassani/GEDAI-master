72a7643d02241d3da9be82018496bb8d5fab0799
NeuroTuning Lab <tomas.ros@unige.ch>
2026-05-11 23:33:45 +0200

    adjust leadfield use
    Update GEDAI.m to remove an unused full_leadfield_matrix initialization and adjust which dipfit leadfield call is active (use the Colin27 leadfield variant, while commenting the tess_cortex line). Minor cleanup and whitespace adjustments included.

--- GEDAI.m
+++ GEDAI.m
@@ -175,15 +175,10 @@ else
 end
 
 %% Create Reference Covariance Matrix (refCOV)
-full_leadfield_matrix = []; % Initialize empty leadfield
+
 if ~ischar(ref_matrix_type)
     refCOV = ref_matrix_type; % Use custom covariance matrix
     disp([newline 'Using custom covariance matrix']);
-    % 
-    % if strcmp(signal_type, 'meg')
-    %  disp([newline 'Normalizing MEG gram matrix']);
-    % refCOV=corrcov(refCOV);
-    % end
 
  
 else
@@ -249,7 +244,6 @@ else
          error(['CRITICAL: Channel locations are incomplete. ' ...
                'Ensure all %d channels have X, Y, Z and spherical coordinates.'], num_chans);
     
-
     end
 
 
@@ -269,8 +263,8 @@ else
     %  Boundary Element Method (BEM) head model based on EEGLAB/Fieldtrip source model, see https://eeglab.org/tutorials/09_source/Model_Settings.html
     [~, chanlocs_transform] = coregister(EEGin.chanlocs, 'standard_1005.elc','warp', 'auto', 'manual', 'off');
     EEGin = pop_dipfit_settings(EEGin, 'hdmfile','standard_vol.mat','mrifile','standard_mri.mat','chanfile','standard_1005.elc','coordformat','MNI','coord_transform',chanlocs_transform);
-    EEGin = pop_leadfield(EEGin, 'sourcemodel','tess_cortex_mid_low_2000V.mat','sourcemodel2mni',[0 -24 -45 0 0 -1.5708 1000 1000 1000] ,'downsample',1); %  Surface ICBM152
-    % EEGin = pop_leadfield(EEGin, 'sourcemodel','head_modelColin27_5003_Standard-10-5-Cap339.mat','sourcemodel2mni',[0 -24 -45 0 0 -1.5708 1000 1000 1000] ,'downsample',1); % Surface Colin27
+    % EEGin = pop_leadfield(EEGin, 'sourcemodel','tess_cortex_mid_low_2000V.mat','sourcemodel2mni',[0 -24 -45 0 0 -1.5708 1000 1000 1000] ,'downsample',1); %  Surface ICBM152
+    EEGin = pop_leadfield(EEGin, 'sourcemodel','head_modelColin27_5003_Standard-10-5-Cap339.mat','sourcemodel2mni',[0 -24 -45 0 0 -1.5708 1000 1000 1000] ,'downsample',1); % Surface Colin27
     % EEGin = pop_leadfield(EEGin, 'sourcemodel','LORETA-Talairach-BAs.mat','sourcemodel2mni',[],'downsample',1); % Volumetric ICBM152
     
     DIPFIT_leadfield=cell2mat(EEGin.dipfit.sourcemodel.leadfield); %Gain matrix
@@ -284,7 +278,7 @@ else
          error(['CRITICAL: Channel locations are incomplete. ' ...
                'Ensure all %d channels have X, Y, Z and spherical coordinates.'], num_chans);
    end
-end
+    end
 
 
 % --- Wavelet-based High-Pass Filtering ---
