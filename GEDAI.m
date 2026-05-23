b8207a05645f1050d5e9fc079d47c83b0aebcf7d
NeuroTuning Lab <tomas.ros@unige.ch>
2026-05-23 18:14:30 +0200

    Update GEDAI.m

--- GEDAI.m
+++ GEDAI.m
@@ -603,7 +603,7 @@ end
     disp([newline 'SENSAI threshold detection...please wait']);
     broadband_optimization_type = 'parabolic';
     broadband_artifact_threshold_type = 'auto-';
-    broadband_minThreshold = -2;
+    broadband_minThreshold = 0;
     broadband_maxThreshold = 12;
     [cleaned_broadband_data, ~, broadband_sensai, broadband_thresh, broadband_ENOVA] = GEDAI_per_band(double(EEGavRef.data), EEGavRef.srate, EEGavRef.chanlocs, broadband_artifact_threshold_type, broadband_epoch_size, refCOV, broadband_optimization_type, parallel, signal_type, broadband_minThreshold, broadband_maxThreshold, smoothing_window_seconds);
     SENSAI_score_per_band = broadband_sensai;
